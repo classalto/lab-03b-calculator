db68d0a370ff368f42a979372a0d0d9c43243069
VICTOR WILLIAMS <vbw293@gmail.com>
2021-01-08 14:04:38 -0800

    remove code from example.test.js

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,5 @@
 import './example.test.js';
-import { getSum, getSubtract, getDivision, getMultiply } from '../utils.js'
+import { getSum, getSubtract, getDivision, getMultiply } from '../utils.js';
 
 const test = QUnit.test;
 
@@ -9,25 +9,25 @@ test('should return 0 when given 1, -1 ', (expect) => {
     const actual = getSum(1, -1);
 
     expect.equal(actual, expected);
-}
+});
 test('should return -1 when given 1, -2 ', (expect) => {
-    const expected = -1;
+    const expected = 3;
 
     const actual = getSubtract(1, -2);
 
     expect.equal(actual, expected);
-}
+});
 test('should return 0 when given 1, 0 ', (expect) => {
-    const expected = 0;
+    const expected = Infinity;
 
     const actual = getDivision(1, 0);
 
     expect.equal(actual, expected);
-}
+});
 test('should return 0 when given 1, 0 ', (expect) => {
     const expected = 0;
 
     const actual = getMultiply(1, 0);
 
     expect.equal(actual, expected);
-}
+});

c14ac0aa1a464ebb57f74f418f0f3f37f524a0f1
VICTOR WILLIAMS <vbw293@gmail.com>
2021-01-08 12:04:57 -0800

    add refactor and appropriate utils.js and handlers.js

---
+++ utils.js
@@ -0,0 +1,20 @@
+/* eslint-disable require-jsdoc */
+// addition function
+export function getSum(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+};
+
+// subtraction function
+export function getSubtract(numberOne, numberTwo) {
+  return numberOne - numberTwo;
+};
+
+// division function
+export function getDivision(numberOne, numberTwo) {
+  return numberOne / numberTwo;
+};
+
+// multiplication function
+export function getMultiply(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+};
